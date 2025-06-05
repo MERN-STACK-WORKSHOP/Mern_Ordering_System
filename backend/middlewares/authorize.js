67a60a738df78bca857b3404f54cc08c516b3e19
M40 <modev.404@gmail.com>
2025-06-05 16:26:11 +0300

    create casl ability and middleware

--- backend/middlewares/authorize.js
+++ backend/middlewares/authorize.js
@@ -1,8 +1,12 @@
+const { ForbiddenError } = require("@casl/ability");
+const defineAbilitiesFor = require("../utils/ability");
+
 /**
  * @param {...string} allowedRoles
  * @returns {function} middleware
- * @description Middleware to check if the user has the required role
+ * @description Middleware to check if the user has the required role using custom logic
  */
+
 const authorize = (...allowedRoles) => {
   return (req, res, next) => {
     if (!allowedRoles.includes(req.user.role)) {
@@ -12,4 +16,31 @@ const authorize = (...allowedRoles) => {
     }
     next();
   };
+};
+
+/**
+ * @param {string} action
+ * @param {string} subject
+ * @returns {function} middleware
+ * @description Middleware to check if the user has the required permission using casl
+ */
+const authorizeAbility = (action, subject) => {
+  return async (req, res, next) => {
+    try {
+      const ability = defineAbilitiesFor(req.user);
+      ForbiddenError.from(ability).throwUnlessCan(action, subject);
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(403).json({
+        error: "Forbidden",
+        message: "You don't have permission to access this resource",
+      });
+    }
+  };
+};
+
+module.exports = {
+  authorize,
+  authorizeAbility,
 };
