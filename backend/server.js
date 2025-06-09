18286fa64e9fff973b91bc5dd3c895bb9193f60c
M40 <modev.404@gmail.com>
2025-06-09 14:04:29 +0300

    seed products & intall node-fetch

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const rateLimit = require("express-rate-limit");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/user.route");
-const { seedCategories } = require("./utils/seed");
+const { seedCategories, seedProducts } = require("./utils/seed");
 
 app.use(express.json());
 app.use(cors());
@@ -25,8 +25,9 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/seed", (req, res) => {
-  seedCategories();
+app.get("/seed", async (req, res) => {
+  // seedCategories();
+  await seedProducts();
   res.json({ message: "Seeded successfully" });
 });
 
--- backend/utils/seed.js
+++ backend/utils/seed.js
@@ -1,10 +1,12 @@
 const Category = require("../models/category.schema");
+const Product = require("../models/product.schema");
+const fetch = require("node-fetch");
 
-const BASE_URL = "https://fakestoreapi.in";
+const BASE_URL = "https://fakestoreapi.in/api";
 
 /**
- * Seed category data for the database
- * @description This function seeds the database with category data loaded from a fakestoreapi.in
+ * Seed category and product data for the database
+ * @description This function seeds the database with category and product data loaded from a fakestoreapi.in
  */
 
 const seedCategories = async () => {
@@ -28,6 +30,36 @@ const seedCategories = async () => {
   }
 };
 
+const seedProducts = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/products`);
+    const categories = await Category.find();
+    const { products } = await response.json();
+
+    const productsList = products.map((product) => {
+      const category = categories.find(
+        (category) => category.name === product.category
+      );
+      return {
+        name: product.title,
+        description: product.description,
+        imageUrl: product.image,
+        price: product.price,
+        stock: product.stock,
+        discount: product.discount,
+        tags: product.tags,
+        category: category._id,
+      };
+    });
+
+    await Product.insertMany(productsList);
+    console.log("Products seeded successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 module.exports = {
   seedCategories,
+  seedProducts,
 };
