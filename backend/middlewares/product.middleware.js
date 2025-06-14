646c268aa16e2ceb363a24652b0c75e927ee00d8
Mohamed Bashiir Hassan <Mohabashka18gmail.com@>
2025-06-14 13:42:19 +0300

    Added Product Validation Middleware

---
+++ backend/middlewares/product.middleware.js
@@ -0,0 +1,16 @@
+
+
+
+const productValidation = require("../validations/product.validation")
+
+const productValidationMiddleware = (req,res,next)=>{
+    const { error } = productValidation.validate(req.body)
+  if (error) {
+    return res
+      .status(400)
+      .json({ message: "Validation error:".concat(error.details[0].message) });
+  }
+  next();
+} 
+
+module.exports = productValidationMiddleware
